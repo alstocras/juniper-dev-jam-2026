f672c2f80e28000f0914e70253a725c2eee15050
alstocras <alstoperiment@gmail.com>
2026-06-20 13:19:32 +0000

    ugh godot hates me

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -7,4 +7,4 @@ var obj;
 func _process(delta: float) -> void:
 	mousePos = $TileMapLayer.get_local_mouse_position();
 	if Input.is_action_just_pressed("placeObj"):
-		$TileMapLayer.set_cell($TileMapLayer.local_to_map(mousePos), 1, Vector2i(-1, -1), 0)
+		$TileMapLayer.set_cell($TileMapLayer.local_to_map(mousePos), 1, Vector2i(0, 0), 0)
