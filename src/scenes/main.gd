b5d73a99288ccfbf0b495dc3f2e965e0d3390e8e
alstocras <alstoperiment@gmail.com>
2026-06-20 14:40:34 +0000

    organized tilemaps

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,10 +1,10 @@
 extends Node2D;
 
 var mousePos;
-@onready var tMap = $TileMapLayer
+@onready var tMap = $Buildings
 
 func _process(delta: float) -> void:
 	mousePos = tMap.get_local_mouse_position();
 	if Input.is_action_just_pressed("placeObj"):
 		if not tMap.get_cell_source_id(tMap.local_to_map(mousePos)) == 1:
-			tMap.set_cell(tMap.local_to_map(mousePos), 1, Vector2i(0, 0), 1)
+			tMap.set_cell(tMap.local_to_map(mousePos), 0, Vector2i(0, 0), 1)
