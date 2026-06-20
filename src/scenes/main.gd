6dec10e82a3f7548175890308fa11a1e6ed04afa
alstocras <alstoperiment@gmail.com>
2026-06-20 14:23:16 +0000

    variables

--- src/scenes/main.gd
+++ src/scenes/main.gd
@@ -1,9 +1,10 @@
 extends Node2D;
 
 var mousePos;
+@onready var tMap = $TileMapLayer
 
 func _process(delta: float) -> void:
-	mousePos = $TileMapLayer.get_local_mouse_position();
+	mousePos = tMap.get_local_mouse_position();
 	if Input.is_action_just_pressed("placeObj"):
-		if not $TileMapLayer.get_cell_source_id($TileMapLayer.local_to_map(mousePos)) == 1:
-			$TileMapLayer.set_cell($TileMapLayer.local_to_map(mousePos), 1, Vector2i(0, 0), 1)
+		if not tMap.get_cell_source_id(tMap.local_to_map(mousePos)) == 1:
+			tMap.set_cell(tMap.local_to_map(mousePos), 1, Vector2i(0, 0), 1)
