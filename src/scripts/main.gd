ab05a51c432f717a1b4d49676d5566115bebefab
alstocras <alstoperiment@gmail.com>
2026-06-21 09:51:52 +0000

    extracts w/o mouse on top

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -5,7 +5,7 @@ var activeCell;
 @onready var tMap = $Buildings;
 @onready var terrain = $Terrain;
 @onready var bosonLabel = $CanvasLayer/Bosons;
-var bosonCount: int;
+var bosonCount: float;
 
 func _process(delta: float) -> void:
 	mousePos = tMap.get_local_mouse_position();
@@ -13,9 +13,11 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("placeObj"):
 		if not tMap.get_cell_source_id(activeCell) == 1:
 			tMap.set_cell(activeCell, 0, Vector2i(0, 0), 1)
-	if terrain.get_cell_source_id(activeCell) == 2 and tMap.get_cell_source_id(activeCell) == 0:
-			print("extracting...");
-			bosonCount += 1
+	for x in range(-10, 10):
+		for y in range(-10, 10):
+			if terrain.get_cell_source_id(Vector2i(x, y)) == 2 and tMap.get_cell_source_id(Vector2i(x, y)) == 0:
+				print("extracting...");
+				bosonCount += 0.01
 	if Input.is_action_just_pressed("removeObj"):
 		tMap.erase_cell(activeCell)
-	bosonLabel.text = "bosons: " + str(bosonCount)
+	bosonLabel.text = "bosons: " + str(round(bosonCount))
