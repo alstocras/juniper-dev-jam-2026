aaf67375e6e773e8a783db3328883df0b9cc9a5a
alstocras <alstoperiment@gmail.com>
2026-06-22 04:43:37 +0000

    added simpler UI

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -23,14 +23,14 @@ func _process(delta: float) -> void:
 			if terrain.get_cell_source_id(Vector2i(x, y)) == 2 and tMap.get_cell_source_id(Vector2i(x, y)) == 0:
 				var spin = bosonSpins.pick_random();
 				bosonCount += (0.1 * spin);
-	bosonLabel.text = "bosons: " + str(round(bosonCount))
+	bosonLabel.text = str(round(bosonCount))
 	
 	for x in range(-lookrange, lookrange):
 		for y in range(-lookrange, lookrange):
 			if terrain.get_cell_source_id(Vector2i(x, y)) == 1 and tMap.get_cell_source_id(Vector2i(x, y)) == 0:
 				var spin = fermionSpins.pick_random();
 				fermionCount += (0.1 * spin);
-	fermionLabel.text = "fermions: " + str(round(fermionCount))
+	fermionLabel.text = str(round(fermionCount))
 	
 	if Input.is_action_pressed("removeObj"):
 		tMap.erase_cell(activeCell)
