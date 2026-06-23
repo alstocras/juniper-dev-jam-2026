585bc644d236ee67865f7a1a25431669a4a8caf0
alstocras <alstoperiment@gmail.com>
2026-06-23 15:10:11 +0000

    made tunneler extractor scene

--- src/scripts/tunneler-extractor.gd
+++ src/scripts/tunneler-extractor.gd
@@ -10,19 +10,20 @@ var fermionSpins: Array = [0.5, 1.5]
 
 func _physics_process(delta: float) -> void:
 	var oreBelowMe = terrain.local_to_map(global_position)
-	if Global.energyAvailable >= 2 and canExtract:
+	if Global.energyAvailable >= 3 and canExtract and Global.totalFermions >= 5:
 		if not $AnimatedSprite2D.is_playing():
 			$AnimatedSprite2D.play("extract");
 		if terrain.get_cell_source_id(oreBelowMe) == 2 and tMap.get_cell_source_id(oreBelowMe) == 0:
 			var spin = bosonSpins.pick_random();
-			Global.totalBosons += (1 * spin);
+			Global.totalBosons += (5 * spin);
 		elif terrain.get_cell_source_id(oreBelowMe) == 1 and tMap.get_cell_source_id(oreBelowMe) == 0:
 			var spin = fermionSpins.pick_random();
-			Global.totalFermions += (1 * spin);
+			Global.totalFermions += (5 * spin);
 		elif terrain.get_cell_source_id(oreBelowMe) == 3 and tMap.get_cell_source_id(oreBelowMe) == 0:
-			Global.totalIron += 1;
+			Global.totalIron += 5;
 		
 		Global.energyAvailable -= 2;
+		Global.totalFermions -= 5
 		canExtract = false
 		await get_tree().create_timer(1.0).timeout
 		canExtract = true
