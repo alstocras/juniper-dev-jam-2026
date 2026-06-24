f3375c66954528ddfecff3609f7e3a7464ece013
alstocras <alstoperiment@gmail.com>
2026-06-24 11:50:14 +0000

    drill for elec extract

--- src/scripts/electric-extractor.gd
+++ src/scripts/electric-extractor.gd
@@ -4,6 +4,7 @@ var terrain = Global.terrainMap;
 var tMap = Global.buildingMap;
 
 var canExtract: bool = true;
+@onready var sound = $AudioStreamPlayer2D;
 
 var bosonSpins: Array = [0, 1, 2];
 var fermionSpins: Array = [0.5, 1.5]
@@ -21,6 +22,9 @@ func _physics_process(delta: float) -> void:
 			Global.totalFermions += (1 * spin);
 		elif terrain.get_cell_source_id(oreBelowMe) == 3 and tMap.get_cell_source_id(oreBelowMe) == 0:
 			Global.totalIron += 1;
+		sound.play();
+	if not canExtract:
+		sound.stop();
 		
 		Global.energyAvailable -= 2;
 		canExtract = false
