128baf0bac1365299a7ff5a6e3d747f2713ab8a8
alstocras <alstoperiment@gmail.com>
2026-06-26 06:12:48 +0000

    better menu

--- src/scripts/main-menu.gd
+++ src/scripts/main-menu.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/main.tscn");
-
+	
+	
 func _on_quit_pressed() -> void:
-	get_tree().quit();
+	get_tree().quit()
